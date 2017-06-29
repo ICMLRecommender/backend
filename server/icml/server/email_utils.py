aeb77b2b5fa0062fad80e970444729c254d06c73
hptruong93 <hptruong93@gmail.com>
2017-06-29 05:32:36 -0400

    Change to use the verified domain

--- server/icml/server/email_utils.py
+++ server/icml/server/email_utils.py
@@ -2,21 +2,18 @@ import requests
 
 def send_test_message():
     return requests.post(
-        "https://api.mailgun.net/v3/sandboxd0c8caef9e7c4fabbfddc3a36b7de13a.mailgun.org/messages",
+        "https://api.mailgun.net/v3/icml.papro.org.uk/messages",
         auth=("api", "key-2a940124dc9ca22210d6278b6d0f12bd"),
-        data={"from": "Mailgun Sandbox <postmaster@sandboxd0c8caef9e7c4fabbfddc3a36b7de13a.mailgun.org>",
-              "to": "HP Truong <hptruong93@gmail.com>",
+        data={"from": "Mailgun Test <postmaster@icml.papro.org.uk>",
+              "to": ["hptruong93@gmail.com"],
               "subject": "Hello HP Truong",
               "text": "Congratulations HP Truong, you just sent an email with Mailgun!  You are truly awesome!"})
 
-
-
 def send_email(dest, subject, text):
     return requests.post(
-        "https://api.mailgun.net/v3/sandboxd0c8caef9e7c4fabbfddc3a36b7de13a.mailgun.org/messages",
+        "https://api.mailgun.net/v3/icml.papro.org.uk/messages",
         auth=("api", "key-2a940124dc9ca22210d6278b6d0f12bd"),
-        data={"from": "Mailgun Sandbox <postmaster@sandboxd0c8caef9e7c4fabbfddc3a36b7de13a.mailgun.org>",
+        data={"from": "ICML team <postmaster@icml.papro.org.uk>",
               "to": "<{}>".format(dest),
               "subject": subject,
               "text": text})
-
